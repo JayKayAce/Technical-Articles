c78dd685f34ad919b0a070d225c80e269f3d9466
JayKayAce <jesper.kjaer.soerensen@gmail.com>
2020-08-10 21:02:15 +0200

    Fixed mybuild specification Added requirement spec file

--- tutorials/Desktop Python Application/code/mybuild.py
+++ tutorials/Desktop Python Application/code/mybuild.py
@@ -1,11 +1,10 @@
 import PyInstaller.__main__
-import os
 
 install_config = [
-    "name=todo",
+    "todo.py",
+    "-n todo",
     "--clean",
     "--onefile",
-    "scriptname todo.py"
 ]
 
-PyInstaller.__main__.run(pyi_args=install_config)
+PyInstaller.__main__.run(install_config)
