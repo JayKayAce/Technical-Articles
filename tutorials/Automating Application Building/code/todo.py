119e3c1a7c7b529c3097e2171df1158a706e21ec
JayKayAce <jesper.kjaer.soerensen@gmail.com>
2020-09-07 20:04:13 +0200

    Cleared blank spaces from Black

--- tutorials/Automating Application Building/code/todo.py
+++ tutorials/Automating Application Building/code/todo.py
@@ -2,15 +2,13 @@
 Welcome to the ToDo application, Type Q to Quit
 """
 
-
 def main():
-    print(__doc__)  #
+    print(__doc__)
     while True:
         command = input("ToDo>")
         if command == "Q":
             break
     print("Exiting ToDo")
-
 
 if __name__ == "__main__":
     main()
