9797af9b7bf02cc5e02b1dee98d3b0300e8e61ae
JayKayAce <jesper.kjaer.soerensen@gmail.com>
2020-09-07 20:03:40 +0200

    Ran Black on code files.

--- tutorials/Automating Application Building/code/mybuild.py
+++ tutorials/Automating Application Building/code/mybuild.py
@@ -1,5 +1,4 @@
 import PyInstaller.__main__
-#import os
 
 install_config = [
     "-n=todo",
--- tutorials/Automating Application Building/code/todo.py
+++ tutorials/Automating Application Building/code/todo.py
@@ -2,13 +2,15 @@
 Welcome to the ToDo application, Type Q to Quit
 """
 
+
 def main():
-  print(__doc__) # 
-  while True:
-    command = input("ToDo>")
-    if command == "Q":
-      break
-  print("Exiting ToDo")
+    print(__doc__)  #
+    while True:
+        command = input("ToDo>")
+        if command == "Q":
+            break
+    print("Exiting ToDo")
+
 
 if __name__ == "__main__":
-  main()
+    main()
